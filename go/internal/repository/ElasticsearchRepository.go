4416bc12de2896c80468017b1342eecf8faf0867
dimaspriyo <dimspriyo@gmail.com>
2021-03-19 23:54:31 +0700

    Add ESConnection function

--- go/internal/repository/ElasticsearchRepository.go
+++ go/internal/repository/ElasticsearchRepository.go
@@ -1,1 +1,16 @@
 package repository
+
+import (
+	"log"
+
+	"github.com/elastic/go-elasticsearch/v7"
+)
+
+func ESConnection() (es *elasticsearch.Client, err error) {
+	es, err = elasticsearch.NewDefaultClient()
+	if err != nil {
+		log.Fatalf("Error creating the client: %s", err)
+	}
+
+	return es, err
+}
