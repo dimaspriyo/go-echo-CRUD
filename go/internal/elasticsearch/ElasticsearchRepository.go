0cebff6acee09505ff3aeb900dae524e83c0d8a4
dimaspriyo <dimspriyo@gmail.com>
2021-03-30 10:09:04 +0700

    Add postgresql

---
+++ go/internal/elasticsearch/ElasticsearchRepository.go
@@ -0,0 +1,34 @@
+package elasticsearch
+
+import (
+	"github.com/elastic/go-elasticsearch/v7"
+)
+
+type IElasticsearchRepository interface {
+	Log(es *elasticsearch.Client) error
+}
+
+type ElasticRepository struct {
+}
+
+// func (r ElasticRepository) Log(es *elasticsearch.Client, c echo.Context) {
+// 	result, err := ipinfo.ForeignIP(c.Request().RemoteAddr)
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 	}
+
+// 	var Loc = strings.Split(result.Loc, ",")
+
+// 	var myLog = MyLog{
+// 		HostName: result.Hostname,
+// 		IP: IP{
+// 			IPv4: result.IP,
+// 		},
+// 		Location: Location{
+// 			Latitude:  Loc[0],
+// 			Longitude: Loc[1],
+// 		},
+// 	}
+
+// 	res, err := es.Index("MyLog")
+// }
