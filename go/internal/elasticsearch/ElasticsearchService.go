d3e079453d4444a6dedac7d5ad487eebd96d5226
dimaspriyo <dimspriyo@gmail.com>
2021-06-10 17:21:51 +0700

    Add empty ElasaticsearchService

--- go/internal/elasticsearch/ElasticsearchService.go
+++ go/internal/elasticsearch/ElasticsearchService.go
@@ -1,1 +1,34 @@
 package elasticsearch
+
+import (
+	"go/internal/config"
+
+	"github.com/labstack/echo/v4"
+)
+
+type IElasticsearchService interface {
+	getByName(name string, ctx echo.Context)
+
+	save(name string, ctx echo.Context)
+}
+
+func newElasticsearchService(s *config.GlobalShared) IElasticsearchService {
+
+	return ElasticsearchService{
+		shared: s,
+	}
+
+}
+
+type ElasticsearchService struct {
+	shared *config.GlobalShared
+	repo   IElasticsearchRepository
+}
+
+func (s ElasticsearchService) getByName(name string, ctx echo.Context) {
+
+}
+
+func (s ElasticsearchService) save(name string, ctx echo.Context) {
+
+}
